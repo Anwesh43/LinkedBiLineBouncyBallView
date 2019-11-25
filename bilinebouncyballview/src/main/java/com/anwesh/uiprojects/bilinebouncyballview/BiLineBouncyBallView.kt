302235128ccd0ab60063a125bec91c59633e44f8
Anwesh43 <anweshthecool0@gmail.com>
2019-11-25 09:23:48 +0530

    adding constants and libraries

--- bilinebouncyballview/src/main/java/com/anwesh/uiprojects/bilinebouncyballview/BiLineBouncyBallView.kt
+++ bilinebouncyballview/src/main/java/com/anwesh/uiprojects/bilinebouncyballview/BiLineBouncyBallView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bilinebouncyballview
 /**
  * Created by anweshmishra on 25/11/19.
  */
+
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
